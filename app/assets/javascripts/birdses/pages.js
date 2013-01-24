bd880698da9cd506e13a242162e79561de431757
Maximilian Haack <mxhaack@gmail.com>
2013-01-24 11:06:14 +0100

    namespace under css ids under birdses_

--- app/assets/javascripts/birdses/pages.js
+++ app/assets/javascripts/birdses/pages.js
@@ -8,26 +8,26 @@ $(document).ready(function() {
 
       var name = $('h2').html();
       if ('New Page' == name) {
-        name = $('#page_name').val()
+        name = $('#birdses_page_name').val()
       }
 
       // Toggle the title of the Preview Button
-      $('#editor').toggle();
-      $('#preview').toggle(10, function(){
-        if ($('#preview').is(':visible')) {
+      $('#birdses_editor').toggle();
+      $('#birdses_preview').toggle(10, function(){
+        if ($('#birdses_preview').is(':visible')) {
           preview.val('Edit');
           $.ajax({
             type: "POST",
-            url: $("#preview_form").attr("action"),
+            url: $("#birdses_preview_form").attr("action"),
             data: {
               format: "json",
             utf8: "✓",
-            authenticity_token: $("#preview_form input[name=authenticity_token]").val,
+            authenticity_token: $("#birdses_preview_form input[name=authenticity_token]").val,
             md_content: textarea.val(),
             name: name
             },
             success: function(data, textStatus, jqXHR) {
-              return $("#preview").html(data["html"]);
+              return $("#birdses_preview").html(data["html"]);
             },
             error: function(jqXHR, textStatus, errorThrown) {},
             dataType: "text json"
